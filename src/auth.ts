91082666ef7a7acaf9a2eb3d6becdd2e243dc51f
Hrvoje <hrvoje55karlovic@gmail.com>
2024-02-23 13:04:58 +0100

    Auth.ts bug fix

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/GitHub";
+import GitHub from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth } = NextAuth({
